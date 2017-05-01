96a74608ea00a9598a7f968f07816920228695fb
kyunga <dnldzmdnldzm@naver.com>
2017-05-01 12:19:15 +0900

    -

---
+++ src/main/java/com/dgit/domain/LoginDTO.java
@@ -0,0 +1,27 @@
+package com.dgit.domain;
+
+public class LoginDTO {
+	
+	private String userid;
+	private String userpw;
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUserpw() {
+		return userpw;
+	}
+	public void setUserpw(String userpw) {
+		this.userpw = userpw;
+	}
+	@Override
+	public String toString() {
+		return "LoginDTO [userid=" + userid + ", userpw=" + userpw + "]";
+	}
+	
+	
+	
+}
